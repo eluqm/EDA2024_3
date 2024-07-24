a99f23f4385cc4fcbd7791150938da34b50c418f
Diego <138242587+Gocardi@users.noreply.github.com>
2024-07-23 20:20:50 -0500

    se implemento list.java

---
+++ testpl/estructuras/List.java
@@ -0,0 +1,75 @@
+package estructuras;
+
+public class List<T> {
+    private Node<T> head;
+
+    public List() {
+        this.head = null;
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public boolean remove(T data) {
+        if (head == null) {
+            return false;
+        }
+        if (head.data.equals(data)) {
+            head = head.next;
+            return true;
+        }
+        Node<T> current = head;
+        while (current.next != null && !current.next.data.equals(data)) {
+            current = current.next;
+        }
+        if (current.next != null) {
+            current.next = current.next.next;
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        int count = 0;
+        Node<T> current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public void printList() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
