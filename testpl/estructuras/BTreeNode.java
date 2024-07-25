86c5bc59508ffc490b6f3a11493819945fe2ff4d
Diego <138242587+Gocardi@users.noreply.github.com>
2024-07-25 00:07:04 -0500

    node

--- testpl/estructuras/BTreeNode.java
+++ testpl/estructuras/BTreeNode.java
@@ -1,72 +1,72 @@
-class BTreeNode {
-    int t;  // Minimum degree (defines the range for number of keys)
-    DynamicArray<Song> keys;  // An array of keys
-    DynamicArray<BTreeNode> children;  // An array of child pointers
-    int n;  // Current number of keys
-    boolean leaf;  // Is true when node is leaf. Otherwise false
+package estructuras;
 
-    BTreeNode(int t, boolean leaf) {
+public class BTreeNode<T extends Comparable<T>> {
+    int t;  // Minimum degree
+    DynamicArray<T> keys;
+    DynamicArray<BTreeNode<T>> children;
+    boolean leaf;
+
+    public BTreeNode(int t, boolean leaf) {
         this.t = t;
         this.leaf = leaf;
         this.keys = new DynamicArray<>();
         this.children = new DynamicArray<>();
-        this.n = 0;
     }
 
-    void insertNonFull(Song k) {
-        int i = n - 1;
+    public void insertNonFull(T key) {
+        int i = keys.getSize() - 1;
 
         if (leaf) {
-            while (i >= 0 && keys.get(i).compareTo(k) > 0) {
+            keys.add(null);  // Add a placeholder for the new key
+
+            while (i >= 0 && keys.get(i).compareTo(key) > 0) {
+                keys.set(i + 1, keys.get(i));
                 i--;
             }
-            keys.add(i + 1, k);
-            n++;
+            keys.set(i + 1, key);
         } else {
-            while (i >= 0 && keys.get(i).compareTo(k) > 0) {
+            while (i >= 0 && keys.get(i).compareTo(key) > 0) {
                 i--;
             }
-            if (children.get(i + 1).n == 2 * t - 1) {
-                splitChild(i + 1, children.get(i + 1));
-                if (keys.get(i + 1).compareTo(k) < 0) {
+            i++;
+            if (children.get(i).keys.getSize() == 2 * t - 1) {
+                splitChild(i, children.get(i));
+                if (keys.get(i).compareTo(key) < 0) {
                     i++;
                 }
             }
-            children.get(i + 1).insertNonFull(k);
+            children.get(i).insertNonFull(key);
         }
     }
 
-    void splitChild(int i, BTreeNode y) {
-        BTreeNode z = new BTreeNode(y.t, y.leaf);
-        z.n = t - 1;
-
+    public void splitChild(int i, BTreeNode<T> y) {
+        BTreeNode<T> z = new BTreeNode<>(y.t, y.leaf);
         for (int j = 0; j < t - 1; j++) {
-            z.keys.add(y.keys.get(j + t));
+            z.keys.add(y.keys.get(t));
+            y.keys.remove(t);
         }
-
         if (!y.leaf) {
             for (int j = 0; j < t; j++) {
-                z.children.add(y.children.get(j + t));
+                z.children.add(y.children.get(t));
+                y.children.remove(t);
             }
         }
-
-        for (int j = n; j >= i + 1; j--) {
-            children.add(j + 1, children.get(j));
+        children.add(null);
+        for (int j = children.getSize() - 1; j > i + 1; j--) {
+            children.set(j, children.get(j - 1));
         }
-
-        children.add(i + 1, z);
-
-        for (int j = n - 1; j >= i; j--) {
-            keys.add(j + 1, keys.get(j));
+        children.set(i + 1, z);
+        keys.add(null);
+        for (int j = keys.getSize() - 1; j > i; j--) {
+            keys.set(j, keys.get(j - 1));
         }
-
-        keys.add(i, y.keys.get(t - 1));
-        n++;
+        keys.set(i, y.keys.get(t - 1));
+        y.keys.remove(t - 1);
     }
 
-    void traverse() {
+    public void traverse() {
         int i;
-        for (i = 0; i < n; i++) {
+        for (i = 0; i < keys.getSize(); i++) {
             if (!leaf) {
                 children.get(i).traverse();
             }
@@ -77,18 +77,17 @@ class BTreeNode {
         }
     }
 
-    BTreeNode search(Song k) {
+    public BTreeNode<T> search(T key) {
         int i = 0;
-        while (i < n && k.compareTo(keys.get(i)) > 0) {
+        while (i < keys.getSize() && key.compareTo(keys.get(i)) > 0) {
             i++;
         }
-        if (i < n && keys.get(i).compareTo(k) == 0) {
+        if (i < keys.getSize() && key.compareTo(keys.get(i)) == 0) {
             return this;
         }
         if (leaf) {
             return null;
         }
-        return children.get(i).search(k);
+        return children.get(i).search(key);
     }
 }
-
