02924542eb59fdc5fbe5d5739eb28f50dc8083c4
Diego <138242587+Gocardi@users.noreply.github.com>
2024-07-25 00:12:25 -0500

    se modifico song

--- testpl/estructuras/Song.java
+++ testpl/estructuras/Song.java
@@ -1,28 +1,30 @@
+package estructuras;
+
 public class Song implements Comparable<Song> {
-    
     private String artistName;
     private String trackName;
     private String trackID;
     private int popularity;
     private short year;
     private String genre;
-    private String danceability;
-    private String energy;
-    private String key;
-    private String loudness;
-    private String mode;
-    private String speechiness;
-    private String acousticness;
-    private String instrumentalness;
-    private String liveness;
-    private String valence;
-    private String tempo;
-    private float duration;
-    private String timeSignature;
-    
-    //artist_name,track_name,track_id,popularity,year,genre,danceability,energy,key,loudness,mode,speechiness,acousticness,instrumentalness,liveness,valence,tempo,duration_ms,time_signature
+    private float danceability;
+    private float energy;
+    private int key;
+    private float loudness;
+    private int mode;
+    private float speechiness;
+    private float acousticness;
+    private float instrumentalness;
+    private float liveness;
+    private float valence;
+    private float tempo;
+    private int durationms;
+    private int timeSignature;
 
-    public Song(String artistName, String trackName, String trackID, int popularity, short year, String genre, String danceability, String energy, String key, String loudness, String mode, String speechiness, String acousticness, String instrumentalness, String liveness, String valence, String tempo, int durationms, String timeSignature) {
+    public Song(String artistName, String trackName, String trackID, int popularity, short year, String genre,
+                float danceability, float energy, int key, float loudness, int mode, float speechiness,
+                float acousticness, float instrumentalness, float liveness, float valence, float tempo,
+                int durationms, int timeSignature) {
         this.artistName = artistName;
         this.trackName = trackName;
         this.trackID = trackID;
@@ -40,64 +42,88 @@ public class Song implements Comparable<Song> {
         this.liveness = liveness;
         this.valence = valence;
         this.tempo = tempo;
-        this.duration = (float) (durationms/60000.0);
+        this.durationms = durationms;
         this.timeSignature = timeSignature;
-    }
-
-    // Para el usuario
-    public Song(String artistName, String trackName, short year) {
-        this.artistName = artistName;
-        this.trackName = trackName;
-        //this.popularity = popularity;
-        this.year = year;
-        //this.duration = (float) (durationms/60000.0);
     }
 
     public String getArtistName() {
         return artistName;
     }
-    public void setArtistName(String artistName) {
-        this.artistName = artistName;
-    }
 
     public String getTrackName() {
         return trackName;
     }
-    public void setTrackName(String trackName) {
-        this.trackName = trackName;
+
+    public String getTrackID() {
+        return trackID;
     }
 
     public int getPopularity() {
         return popularity;
     }
-    public void setPopularity(int popularity) {
-        this.popularity = popularity;
-    }
 
     public short getYear() {
         return year;
     }
-    public void setYear(short year) {
-        this.year = year;
+
+    public String getGenre() {
+        return genre;
     }
 
-    public float getDuration() {
-        return duration;
+    public float getDanceability() {
+        return danceability;
     }
-    public void setDuration(int durationms) {
-        this.duration = (float) (durationms/60000.0);
+
+    public float getEnergy() {
+        return energy;
     }
-    public void setDuration(float duration) {
-        this.duration = duration;
+
+    public int getKey() {
+        return key;
+    }
+
+    public float getLoudness() {
+        return loudness;
+    }
+
+    public int getMode() {
+        return mode;
+    }
+
+    public float getSpeechiness() {
+        return speechiness;
+    }
+
+    public float getAcousticness() {
+        return acousticness;
+    }
+
+    public float getInstrumentalness() {
+        return instrumentalness;
+    }
+
+    public float getLiveness() {
+        return liveness;
+    }
+
+    public float getValence() {
+        return valence;
+    }
+
+    public float getTempo() {
+        return tempo;
+    }
+
+    public int getDurationms() {
+        return durationms;
+    }
+
+    public int getTimeSignature() {
+        return timeSignature;
     }
 
     @Override
-    public String toString() {
-        return "<" + artistName + "> Compuso: <" + trackName + ">";
-    }
-
-    @Override
-    public int compareTo(Song other) {
-        return Short.compare(this.year, other.year);
+    public int compareTo(Song o) {
+        return this.trackID.compareTo(o.trackID);
     }
 }
